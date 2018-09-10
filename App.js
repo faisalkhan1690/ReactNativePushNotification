d0c47aa8972b820f87bccd9ccf30c803af4cbf4c
Faisal Khan <faisal.khan@kelltontech.com>
2018-09-10 16:57:53 +0530

    android setuo done

--- App.js
+++ App.js
@@ -8,22 +8,64 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import FCM,{FCMEvent} from "react-native-fcm";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fcm_token: ""
+    };
+  }
+
+
+  componentDidMount() {
+    
+    // this method generate fcm token.
+    FCM.requestPermissions();
+    FCM.getFCMToken().then(token => {
+      this.setState({fcm_token:token});
+      console.warn("TOKEN (getFCMToken)", token);
+    });
+    
+    // This method get all notification from server side.
+    FCM.getInitialNotification().then(notif => {
+      console.warn("INITIAL NOTIFICATION", notif)
+    });
+    
+    // This method give received notifications to mobile to display.
+    FCM.on(FCMEvent.Notification, notif => {     
+      if (notif && notif.local_notification) {
+        return;
+      }
+      console.warn("a", notif);
+      this.sendRemote(notif);
+    });
+  }
+  
+  // This method display the notification on mobile screen.
+  sendRemote(notif) {
+    var notiObject = {
+      wake_screen: true,
+      priority: "high",
+      show_in_foreground: true,
+      large_icon: "ic_launcher",
+      icon: "ic_launcher",
+      title:"FCM sample",
+      body:notif.fcm.body
+    }
+
+    FCM.presentLocalNotification(notiObject);
+
+  
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to React Native FCM demo</Text>
       </View>
     );
   }
@@ -40,10 +82,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
